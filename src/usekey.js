6ffede98a4f9b600e5ab01d414debe8927453eee
Oyedelehabeeb <hoyedeley4@gmail.com>
2024-06-10 07:47:12 +0100

    MovieRating WebApp with React.js

---
+++ src/usekey.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+export function useKey(key, action) {
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code.toLowerCase() === key.toLocaleLowerCase()) {
+          action();
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [key, action]
+  );
+}
